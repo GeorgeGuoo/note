c7368daf9c0073136921ed59eb194e869c0e9d20
guojingzhong <13877418057@163.com>
2021-07-19 11:46:42 +0800

    init.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  base: '/',
+  port: '9988',
+  title: 'Hello 最光阴',
+  description: '只是玩玩而已',
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    // 导航栏
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'Java',
+        ariaLabel: 'Java Menu',
+        items: [
+          {text: 'Java 基础', link: '/language/chinese/'},
+          {text: '集合', link: '/language/japanese/'},
+          {text: '多线程', link: '/language/japanese/'},
+          {text: 'JVM', link: '/language/japanese/'},
+        ]
+      },
+      {
+        text: 'MySQL',
+        ariaLabel: 'Java Menu',
+        items: [
+          {text: 'Java 基础', link: '/language/chinese/'},
+          {text: '集合', link: '/language/japanese/'},
+          {text: '多线程', link: '/language/japanese/'},
+          {text: 'JVM', link: '/language/japanese/'},
+        ]
+      },
+      {
+        text: 'Spring',
+        ariaLabel: 'Java Menu',
+        items: [
+          {text: 'Java 基础', link: '/language/chinese/'},
+          {text: '集合', link: '/language/japanese/'},
+          {text: '多线程', link: '/language/japanese/'},
+          {text: 'JVM', link: '/language/japanese/'},
+        ]
+      },
+      {
+        text: 'SpringBoot',
+        ariaLabel: 'Java Menu',
+        items: [
+          {text: 'Java 基础', link: '/language/chinese/'},
+          {text: '集合', link: '/language/japanese/'},
+          {text: '多线程', link: '/language/japanese/'},
+          {text: 'JVM', link: '/language/japanese/'},
+        ]
+      },
+      {
+        text: 'SpringBoot',
+        ariaLabel: 'Java Menu',
+        items: [
+          {text: 'Java 基础', link: '/language/chinese/'},
+          {text: '集合', link: '/language/japanese/'},
+          {text: '多线程', link: '/language/japanese/'},
+          {text: 'JVM', link: '/language/japanese/'},
+        ]
+      }
+    ]
+  }
+}
